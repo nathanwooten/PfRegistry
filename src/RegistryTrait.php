187db68e78ac2d56699d380e23b3ab37f6d19f68
nw <55707935+nathanwooten@users.noreply.github.com>
2021-05-29 14:14:42 -0400

    Update RegistryTrait.php

--- src/RegistryTrait.php
+++ src/RegistryTrait.php
@@ -3,6 +3,7 @@
 namespace nathanwooten;
 
 use nathanwooten\RegistryWrapper;
+use nathanwooten\RegistryRegister;
 
 trait RegistryTrait
 {
@@ -11,6 +12,26 @@ trait RegistryTrait
 	{
 
 		return new RegistryWrapper;
+
+	}
+
+	public function register()
+	{
+
+		$registry = $this->getRegistry();
+
+		$name = $this->getClassname();
+		if ( ! $registry->has( $name ) ) {
+
+			$registry->set( $name, $this );
+		}
+
+	}
+
+	public function getClassname()
+	{
+
+		return str_replace( __NAMESPACE__ . '\\', '', static::class );
 
 	}
 
