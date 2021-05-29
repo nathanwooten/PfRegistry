7d450967caf045e3014f8871d9f7826b54525c6a
nw <55707935+nathanwooten@users.noreply.github.com>
2021-05-29 14:26:03 -0400

    Delete required.php

--- required.php
+++
@@ -1,9 +0,0 @@
-
-$dir = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
-require_once $dir . 'RegistryAbstract.php';
-require_once $dir . 'Registry.php';
-require_once $dir . 'RegistryTrait.php';
-require_once $dir . 'RegistryWrapper.php';
-require_once $dir . 'RegistryType.php';
-require_once $dir . 'AutoloaderPackageRegistry.php';
-require_once $dir . 'FilesystemRegistry.php';
